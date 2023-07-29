4e1f5a1b1c2c345b4362184025c09c00dace3d5c
ValeriyVinn <valeriy.all@gmail.com>
2023-07-29 23:13:07 +0300

    first commit

--- js/Homework/JS - 06/task 01.js
+++
@@ -1,8 +0,0 @@
-const listItem = document.querySelectorAll(".item");
-
-console.log("Number of categories:", listItem.length);
-
-listItem.forEach((el) => {
-  console.log("Category:", el.firstElementChild.textContent);
-  console.log("Elements:", el.lastElementChild.children.length);
-});
--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// const checkIfPositive = (number) => {
+//   if(number > 0) {
+//     console.log('number is positive')
+//   } 
+//   else if (number < 0) {
+//     console.log('number is negative')
+//   }
+//   else {
+//     console.log('number = 0')
+//   }
+// }
+
+// const checkIfPositive = (number) => {
+//   const message = number > 0 ? 'number is positive' : number < 0 ? 'number is negative' : 'number = 0';
+//   console.log(message);
+// }
+
+const checkIfPositive = (number) => {
+  switch (Math.sign(number)) {
+    case 1:
+      console.log('number is positive');
+      break;
+    case -1:
+      console.log('number is negative');
+      break;
+    default:
+      console.log('number = 0');
+      break;
+  }
+}
 
 
-console.log(window)
-console.dir(document);
+checkIfPositive(0)
+
