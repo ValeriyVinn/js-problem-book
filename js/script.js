1919fa773d79e82bbadb6d19e6e2670d2f5def7e
ValeriyVinn <valeriy.all@gmail.com>
2023-07-30 18:31:11 +0300

    Added Scroll btn

--- js/script.js
+++ js/script.js
@@ -13,6 +13,40 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// ============= Scroll btn =================
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  // console.log(pos)
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  // console.log(calcHeight)
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  // console.log(scrollValue)
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+
+  scrollProgress.style.background = `conic-gradient(cornflowerblue ${scrollValue}%, aliceblue ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+// ============= Scroll btn =================
+
+
+
+
+
 
 
 // !-------------  Working zone ------------------
