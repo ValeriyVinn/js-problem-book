29658b3779a3f280c3127f1b26919d7a7c3a2bac
ValeriyVinn <valeriy.all@gmail.com>
2023-08-28 21:29:03 +0300

    Add solutions to task  2-03

--- js/script.js
+++ js/script.js
@@ -51,18 +51,14 @@ const increaseByTenPercent = (array) => {
 
 console.log(increaseByTenPercent([1, 2, 3, 4, 5.5]));
 
-
 const firstZeroPosition = (string) => {
   return string.indexOf(0) + 1;
-}
-console.log(firstZeroPosition('dk0kn'))
+};
+console.log(firstZeroPosition("dk0kn"));
 
 const d = "607vv67n3244459";
 const f = d.indexOf(0) + 1;
 console.log(f);
-
-
-
 
 function findLargestNumber() {
   const arr = [...arguments];
@@ -75,11 +71,83 @@ console.log(findLargestNumber(2, 45, 35, 21, 1, 95));
 console.log(findLargestNumber(12, 5, 365, 61, 11, 35));
 
 function getRandomNumbers(min, max, n) {
-  const array = []
-  for (let num = 0; num < n; num+=1) {
-    array.push (Math.round(Math.random() * (max - min) + min))
+  const array = [];
+  for (let num = 0; num < n; num += 1) {
+    array.push(Math.round(Math.random() * (max - min) + min));
   }
-  return array
+  return array;
 }
 
-console.log(getRandomNumbers(1, 100, 10))
+console.log(getRandomNumbers(1, 100, 10));
+
+console.log((1 && 5 && 0) || ("oo" && null) || (0 && "false"));
+
+function generateExpression(operands, operators, comparisons) {
+  let expression = "";
+
+  for (let i = 0; i < comparisons; i++) {
+    expression += operands[i];
+
+    if (i < comparisons - 1) {
+      expression +=
+        " " + operators[Math.floor(Math.random() * operators.length)] + " ";
+    }
+  }
+
+  return expression;
+}
+
+const operands = [0, 34, "abc", 3, false, "welcome", null, "xyz"];
+const operators = ["&&", "||"];
+const comparisons = 7;
+
+const generatedExpression = generateExpression(
+  operands,
+  operators,
+  comparisons
+);
+console.log(generatedExpression);
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+const originalArray = [3, null, "abc", 0, "xyz", false, "welcome", 34];
+const shuffledArray = [...originalArray]; // Створюємо копію масиву для перетасування
+shuffleArray(shuffledArray);
+
+console.log("Оригінальний масив:", originalArray);
+console.log("Перетасований масив:", shuffledArray);
+
+console.log(0 || (34 && "abc" && 3) || false || ("welcome" && null));
+
+const x = (array) => {
+  const y = array.map((el) => el * 2);
+  return y;
+};
+console.log(x([3, 8, 8, 0, 5, 4, 33, 34]));
+
+function removeElements(array, element) {
+  return array.filter((el) => el !== element);
+}
+console.log(
+  removeElements([3, null, "abc", 0, "xyz", false, "welcome", 34], 0)
+);
+
+function removeElement(array, element) {
+  let index = array.length - 1;
+
+  while (index >= 0) {
+    if (array[index] === element) {
+      array.splice(index, 1);
+    }
+    index -= 1;
+  }
+  return array;
+}
+console.log(
+  removeElement([34, 34, "abc", 34, "xyz", false, "welcome", 34], 34)
+);
