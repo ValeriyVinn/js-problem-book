2154e6702d80c550007d7f421a912189c3a4d8c7
ValeriyVinn <valeriy.all@gmail.com>
2023-11-24 19:37:04 +0200

    Added  Homework 7

--- js/blackboard.js
+++ js/blackboard.js
@@ -50,15 +50,7 @@ const getVerbsInColumn = function (string) {
 
 
 // ?------- 6 homework -- write here -------
-
-// const liEl = document.querySelectorAll('.item')
-// console.log('Quantity of category:', liEl.length)
-// console.log('')
-// liEl.forEach((el) => {
-//   console.log('Name of category:', el.firstElementChild.textContent)
-//   console.log('Quantity of subcategory:',el.lastElementChild.children.length)
-//   console.log('')
-// })
+// ! Task 1 ---------------
 
 
 
@@ -71,25 +63,6 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-
-
-// const createList = ingredients.map((el) => {
-//   const listEl = document.createElement('li')
-//   const tittleEl = document.createElement('h3')
-
-//   listEl.classList.add('itemchik')
-//   tittleEl.textContent = el
-//   listEl.append(tittleEl)
-
-//   return listEl
-// })
-
-// const container = document.getElementById('ingredients').append(...createList)
-
-
-
-// ! Task 2 ----------
-
 
 
 // ! Task 3 -------------
@@ -108,7 +81,28 @@ const images = [
   },
 ];
 // img.classList.add('img-properties')
-// ! Task 3 ---------------
+
+
+// ! Task 4 ---------------
+
+
+
+// ! Task 5 ---------------
+
+
+// ! Task 6 ---------------
+
+
+// ! Task 7 ---------------
+
+
+// ! Task 8 ---------------
+
+
+// ! Task 9 ---------------
+
+
+// ! Task 10 ---------------
 
 
 
@@ -123,3 +117,149 @@ const images = [
 
 // ?------- 11 homework -- write here -------
 
+
+//  Контрольна 7  -----------------
+//  Завдання 1 --------------------
+
+
+// import { galleryItems } from "./gallery-items-hw-seven";
+
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+
+
+const galleryReference = document.querySelector(".gallery");
+
+function createGallery(items) {
+  items.map((item) => {
+    const markup = `<div class="gallery__item">
+                      <a class="gallery__link" href="${item.original}">
+                        <img
+                          class="gallery__image"
+                          src="${item.preview}"
+                          data-source="${item.original}"
+                          alt="${item.description}"
+                        />
+                      </a>
+                    </div>`;
+
+    galleryReference.insertAdjacentHTML("beforeend", markup);
+  });
+}
+createGallery(galleryItems);
+
+galleryReference.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImage = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img width="1280" height="854" src="${largeImage}">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+  function closeModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
+}
+
+
+//  Завдання 2 --------------------
+// import { galleryItems } from "./gallery-items.js";
+
+const galleryReference2 = document.querySelector(".gallery-two");
+
+function createGallery2(items) {
+  items.map((item) => {
+    const markup = `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+  </a>`;
+
+    galleryReference2.insertAdjacentHTML("beforeend", markup);
+  });
+}
+
+createGallery2(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
