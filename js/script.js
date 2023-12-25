e33a45f72a174e2c9bbf8b8a801a43fa09904bdd
ValeriyVinn <valeriy.all@gmail.com>
2023-12-25 18:13:46 +0200

    Added dropdown button Markup

--- js/script.js
+++ js/script.js
@@ -13,6 +13,31 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// ============= Click Dropdown Button on Markup =================
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+
+
 // ============= Scroll btn =================
 
 let calcScrollValue = () => {
