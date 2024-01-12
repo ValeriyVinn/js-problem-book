af08128c5e501bdf8a15992a5d53e8187d2fd7a8
ValeriyVinn <valeriy.all@gmail.com>
2024-01-12 21:17:56 +0200

    Refactored markup menu to abstract menu. Added JS items

--- js homeworks/js/js homework.js
+++ js homeworks/js/js homework.js
@@ -1,6 +1,16 @@
 // ?------- 6 homework -- write here -------
 // ! Task 1 ---------------
 
+// const liEl = document.querySelectorAll('.item')
+
+// console.log("Number of categories", liEl.length)
+// console.log("")
+// liEl.forEach((el => {
+//   console.log("Categories: ", el.firstElementChild.textContent)
+//   console.log("Number of subcategories", el.lastElementChild.children.length)
+//   console.log("")
+// }))
+
 // ! Task 2 ---------
 const ingredients = [
     "Potatoes",
@@ -10,21 +20,20 @@ const ingredients = [
     "Herbs",
     "Condiments",
   ];
+
+// const ingredientsEl = document.getElementById("ingredients")
+
+// const markup = ingredients.map((ingredient => {
+//   const li = document.createElement('li')
+//   li.textContent = ingredient
   
-  // const ingredientsRef = document.getElementById('ingredients')
-  // console.log(ingredientsRef)
-  
-  // const markUpIngredients = ingredients.map((e) => {
-    
-  //   const hOne = document.createElement('h1')
-  //   hOne.textContent = e
-  //   console.log(hOne)
-  //   return hOne
-  // })
-  
-  // ingredientsRef.append(...markUpIngredients)
-  
-  
+//   return li
+// }))
+
+
+// ingredientsEl.append(...markup)
+
+
   
   // ! Task 3 -------------
   const images = [
@@ -42,52 +51,51 @@ const ingredients = [
     },
   ];
   
-  // const gallery3 = document.querySelector('.gallery3')
-  // console.log(gallery3)
-  
-  // const mU = images.map(({url, alt}) =>
-  //  `<li><img src="${url}" alt="${alt}" width=300></li>`
-  // ).join('')
-  // gallery3.insertAdjacentHTML('afterbegin', mU)
+const galleryRef = document.querySelector('.gallery3')
+
+// const imagesMarkup = images.map(({url, alt}) => 
+//   `<li><img src="${url}" alt="${alt}" width=300 height=200></li>`
+// ).join('')
+// galleryRef.insertAdjacentHTML("afterbegin", imagesMarkup)
+
   
   // const mU = images.reduce((acc, images) => acc.concat(`<li><img src="${images.url}" alt="${images.alt}" width=300 height=200></li>`),[]).join('')
-  
-  // gallery3.insertAdjacentHTML('afterbegin', mU)
+  // galleryRef.insertAdjacentHTML('afterbegin', mU)
   
   // ! Task 4 ---------------
   
-  // const decrementEl = document.querySelector('[data-action="decrement"]')
-  // const incrementEl = document.querySelector('[data-action="increment"]')
-  // const valueEl = document.querySelector('.span')
-  
-  // let value = 0
-  
-  // decrementEl.addEventListener('click', toDecrease)
-  // incrementEl.addEventListener('click', toIncrease)
-  
-  // function toDecrease(event) {
-  //   value -= 1
-  
-  //   valueEl.textContent = value
-  // }
-  
-  // function toIncrease() {
-  //   value += 1
-  //   valueEl.textContent = value
-  // }
+  const decrementEl = document.querySelector('[data-action="decrement"]')
+const incrementEl = document.querySelector('[data-action="increment"]')
+const valueEl = document.getElementById('value')
+
+let value = 0
+
+decrementEl.addEventListener('click', decrease)
+incrementEl.addEventListener('click', increase)
+
+function decrease (event) {
+  value -= 1
+  valueEl.textContent = value
+}
+
+function increase (event) {
+  value += 1
+  valueEl.textContent = value
+}
+
   
   // ! Task 5 ---------------
   
-  const inputEl = document.getElementById("name-input");
-  const spanEl = document.getElementById("name-output");
+  // const inputEl = document.getElementById("name-input");
+  // const spanEl = document.getElementById("name-output");
   
-  console.log(inputEl);
-  console.log(spanEl);
+  // console.log(inputEl);
+  // console.log(spanEl);
   
-  inputEl.addEventListener("input", (e) => {
-    console.log(e.currentTarget.value);
-    spanEl.textContent = e.currentTarget.value.trim() || "Anonymous";
-  });
+  // inputEl.addEventListener("input", (e) => {
+  //   console.log(e.currentTarget.value);
+  //   spanEl.textContent = e.currentTarget.value.trim() || "Anonymous";
+  // });
   
   // ! Task 6 ---------------
   
@@ -138,147 +146,4 @@ const ingredients = [
   // ! Task 10 ---------------
   
   // ?------- 7 homework -- write here -------
-  
-  //  Контрольна 7  -----------------
-  //  Завдання 1 --------------------
-  
-  // import { galleryItems } from "./gallery-items-hw-seven";
-  
-  const galleryItems = [
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-      description: "Hokkaido Flower",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-      description: "Container Haulage Freight",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-      description: "Aerial Beach View",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-      description: "Flower Blooms",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-      description: "Alpine Mountains",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-      description: "Mountain Lake Sailing",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-      description: "Alpine Spring Meadows",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-      description: "Nature Landscape",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-      description: "Lighthouse Coast Sea",
-    },
-  ];
-  
-  const galleryReference = document.querySelector(".gallery");
-  
-  function createGallery(items) {
-    items.map((item) => {
-      const markup = `<div class="gallery__item">
-                          <a class="gallery__link" href="${item.original}">
-                            <img
-                              class="gallery__image"
-                              src="${item.preview}"
-                              data-source="${item.original}"
-                              alt="${item.description}"
-                            />
-                          </a>
-                        </div>`;
-  
-      galleryReference.insertAdjacentHTML("beforeend", markup);
-    });
-  }
-  createGallery(galleryItems);
-  
-  galleryReference.addEventListener("click", selectImage);
-  
-  function selectImage(event) {
-    event.preventDefault();
-  
-    if (event.target.nodeName !== "IMG") {
-      return;
-    }
-  
-    const largeImage = event.target.dataset.source;
-    const instance = basicLightbox.create(
-      `<img width="1280" height="854" src="${largeImage}">`,
-      {
-        onShow: () => {
-          window.addEventListener("keydown", closeModal);
-        },
-        onClose: () => {
-          window.removeEventListener("keydown", closeModal);
-        },
-      }
-    );
-  
-    function closeModal(event) {
-      if (event.code === "Escape") {
-        instance.close();
-      }
-    }
-    instance.show();
-  }
-  
-  //  Завдання 2 --------------------
-  // import { galleryItems } from "./gallery-items.js";
-  
-  const galleryReference2 = document.querySelector(".gallery-two");
-  
-  function createGallery2(items) {
-    items.map((item) => {
-      const markup = `<a class="gallery__item" href="${item.original}">
-        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
-      </a>`;
-  
-      galleryReference2.insertAdjacentHTML("beforeend", markup);
-    });
-  }
-  
-  createGallery2(galleryItems);
-  
-  const lightbox = new SimpleLightbox(".gallery__item", {
-    captionDelay: 250,
-    captionsData: "alt",
-  });
   
